15a0ec97581be2c4fed3b7ce562cde8d45077103
MuneerAhmed03 <muneerahmed00916@gmail.com>
2024-07-09 23:58:22 +0530

    added second

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css'
 import Home from './pages/home'
+import Second from './pages/second'
 
 function App() {
   
 
   return (
-    <>
-    <Home/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home/>} />
+        <Route path="/second" element={<Second/>} />
+        {/* Redirect to /home as a default route */}
+        <Route path="/" element={<Home/>}>
+        </Route>
+        </Routes>
+    </Router>
   )
 }
 
---
+++ src/pages/second.tsx
@@ -0,0 +1,139 @@
+export default function Component() {
+    return (
+<div className="w-96 h-96 relative bg-white">
+  <div className="w-96 h-36 left-[2px] top-0 absolute">
+    <div className="w-96 h-36 left-0 top-0 absolute bg-white shadow" />
+    <img className="w-20 h-14 left-[20px] top-[40px] absolute" src="https://via.placeholder.com/78x59" />
+    <div className="w-24 h-7 left-[87px] top-[62px] absolute text-center text-black text-lg font-semibold font-['Inter']">MEN</div>
+    <div className="w-24 h-7 left-[551px] top-[62px] absolute text-center text-black text-lg font-semibold font-['Inter']">PEHNAVA</div>
+    <div className="w-24 h-7 left-[597px] top-[45px] absolute text-center text-pink-500 text-sm font-semibold font-['Inter']">NEW</div>
+    <div className="w-24 h-7 left-[455px] top-[62px] absolute text-center text-black text-lg font-semibold font-['Inter']">BEAUTY</div>
+    <div className="w-56 h-7 left-[272px] top-[62px] absolute text-center text-black text-lg font-semibold font-['Inter']">HOME & LIVING</div>
+    <div className="w-24 h-7 left-[237px] top-[62px] absolute text-center text-black text-lg font-semibold font-['Inter']">KIDS</div>
+    <div className="w-24 h-7 left-[162px] top-[62px] absolute text-center text-black text-lg font-semibold font-['Inter']">WOMEN</div>
+    <div className="w-96 h-16 left-[679px] top-[39px] absolute bg-zinc-100 rounded-lg" />
+    <img className="w-11 h-10 left-[696px] top-[54px] absolute" src="https://via.placeholder.com/46x40" />
+    <div className="w-96 h-8 left-[693px] top-[62px] absolute text-center text-neutral-400 text-lg font-normal font-['Inter']">Search for products, brands and more</div>
+    <img className="w-64 h-14 left-[1179px] top-[44px] absolute" src="https://via.placeholder.com/252x57" />
+  </div>
+  <div className="left-[221px] top-[182px] absolute text-center text-pink-500 text-9xl font-semibold font-['Inter']">PEHNAVA</div>
+  <div className="w-96 h-12 left-[65px] top-[1325px] absolute text-black text-4xl font-semibold font-['Inter']">Eras Based Fashion Trends</div>
+  <div className="w-96 h-12 left-[65px] top-[2801px] absolute text-black text-4xl font-semibold font-['Inter']">Customer Survey & Reward Points</div>
+  <div className="w-96 h-96 left-[65px] top-[1476px] absolute">
+    <div className="w-96 h-96 left-0 top-[7px] absolute bg-orange-400" />
+    <div className="w-96 h-96 left-[49px] top-[69px] absolute bg-white" />
+    <div className="h-96 left-[535px] top-[8px] absolute">
+      <div className="w-16 h-16 left-0 top-0 absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[76.68px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[162.33px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[247.97px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[333.62px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[419.26px] absolute bg-black" />
+    </div>
+    <div className="w-16 h-96 left-[1145px] top-[8px] absolute">
+      <div className="w-16 h-16 left-0 top-0 absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[71.55px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[157.01px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[242.47px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[327.93px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[413.39px] absolute bg-black" />
+    </div>
+    <div className="h-96 left-[1755px] top-[5px] absolute">
+      <div className="w-16 h-16 left-0 top-0 absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[77px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[163px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[249px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[335px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[421px] absolute bg-black" />
+    </div>
+    <div className="h-96 left-[2356px] top-[7px] absolute">
+      <div className="w-16 h-16 left-0 top-0 absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[77px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[163px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[249px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[335px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[421px] absolute bg-black" />
+    </div>
+    <div className="h-96 left-[2957px] top-[7px] absolute">
+      <div className="w-16 h-16 left-0 top-0 absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[77px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[163px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[249px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[335px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[421px] absolute bg-black" />
+    </div>
+    <div className="h-96 left-[3558px] top-[4px] absolute">
+      <div className="w-16 h-16 left-0 top-0 absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[77px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[163px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[249px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[335px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[421px] absolute bg-black" />
+    </div>
+    <div className="h-96 left-[4159px] top-[6px] absolute">
+      <div className="w-16 h-16 left-0 top-0 absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[77px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[163px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[249px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[335px] absolute bg-black" />
+      <div className="w-16 h-16 left-0 top-[421px] absolute bg-black" />
+    </div>
+    <div className="w-96 h-96 left-[650px] top-[69px] absolute bg-white" />
+    <div className="w-96 h-96 left-[1269px] top-[64px] absolute bg-white" />
+    <div className="w-96 h-96 left-[3072px] top-[64px] absolute bg-white" />
+    <div className="w-96 h-96 left-[3673px] top-[61px] absolute bg-white" />
+    <div className="w-96 h-96 left-[4274px] top-[63px] absolute bg-white" />
+    <div className="w-96 h-96 left-[1870px] top-[62px] absolute bg-white" />
+    <div className="w-96 h-96 left-[2471px] top-[65px] absolute bg-white" />
+    <div className="left-[149px] top-[160px] absolute text-center text-pink-500 text-6xl font-semibold font-['Inter']">40’s <br/>Fashion</div>
+    <div className="left-[759px] top-[160px] absolute text-center text-pink-500 text-6xl font-semibold font-['Inter']">50’s <br/>Fashion</div>
+    <div className="left-[1369px] top-[162px] absolute text-center text-pink-500 text-6xl font-semibold font-['Inter']">60’s <br/>Fashion</div>
+    <div className="left-[1979px] top-[165px] absolute text-center text-pink-500 text-6xl font-semibold font-['Inter']">70’s <br/>Fashion</div>
+    <div className="left-[2586px] top-[168px] absolute text-center text-pink-500 text-6xl font-semibold font-['Inter']">80’s <br/>Fashion</div>
+    <div className="left-[3187px] top-[167px] absolute text-center text-pink-500 text-6xl font-semibold font-['Inter']">90’s <br/>Fashion</div>
+    <div className="left-[3773px] top-[167px] absolute text-center text-pink-500 text-6xl font-semibold font-['Inter']">2000’s <br/>Fashion</div>
+    <div className="left-[4374px] top-[167px] absolute text-center text-pink-500 text-6xl font-semibold font-['Inter']">GenZ <br/>Fashion</div>
+  </div>
+  <div className="w-96 h-96 left-[65px] top-[554px] absolute">
+    <div className="w-96 h-12 left-0 top-[1px] absolute text-center text-black text-4xl font-semibold font-['Inter']">Location Based Fashion Trends</div>
+    <div className="w-80 h-12 left-[712px] top-0 absolute">
+      <div className="w-72 h-12 left-[34px] top-0 absolute bg-zinc-100 rounded-lg" />
+      <div className="w-64 h-7 left-0 top-[10px] absolute text-center text-black text-2xl font-light font-['Inter']">Select Region</div>
+      <div className="w-8 h-8 left-[257px] top-[41px] absolute" />
+    </div>
+    <div className="w-80 h-12 left-[993px] top-[1px] absolute">
+      <div className="w-52 h-12 left-[94px] top-0 absolute bg-zinc-100 rounded-lg" />
+      <div className="w-64 h-7 left-[34px] top-[9px] absolute text-center text-black text-2xl font-light font-['Inter']">Women</div>
+      <div className="w-8 h-8 left-[257px] top-[41px] absolute" />
+    </div>
+    <div className="w-96 h-96 left-0 top-[151px] absolute bg-zinc-300" />
+    <div className="w-96 h-96 left-[465px] top-[151px] absolute bg-zinc-300" />
+    <div className="w-96 h-96 left-[929px] top-[151px] absolute bg-zinc-300" />
+    <img className="w-96 h-96 left-0 top-[151px] absolute" src="https://via.placeholder.com/374x529" />
+    <div className="w-96 h-44 left-0 top-[510px] absolute bg-black" />
+    <div className="w-80 h-28 left-[13px] top-[542px] absolute text-center text-white text-4xl font-medium font-['Inter']">Assam<br/> Fashion Trends</div>
+    <img className="w-96 h-96 left-[465px] top-[152px] absolute" src="https://via.placeholder.com/372x528" />
+    <div className="w-96 h-44 left-[468px] top-[510px] absolute bg-black" />
+    <div className="w-80 h-28 left-[477px] top-[542px] absolute text-center text-white text-4xl font-medium font-['Inter']">Paris<br/> Fashion Trends</div>
+    <img className="w-96 h-96 left-[930px] top-[152px] absolute" src="https://via.placeholder.com/373x390" />
+    <div className="w-96 h-44 left-[930px] top-[510px] absolute bg-black" />
+    <div className="w-80 h-28 left-[943px] top-[523px] absolute text-center text-white text-4xl font-medium font-['Inter']">Explore New Location Fashion Trends</div>
+  </div>
+  <div className="w-96 h-96 left-[65px] top-[2071px] absolute">
+    <div className="w-96 h-12 left-0 top-0 absolute text-black text-4xl font-semibold font-['Inter']">Connect With Local Sellers</div>
+    <div className="w-36 h-12 left-[1180px] top-[15px] absolute text-black text-3xl font-light font-['Inter']">See More</div>
+    <img className="w-96 h-96 left-0 top-[94px] absolute" src="https://via.placeholder.com/400x543" />
+    <img className="w-96 h-96 left-[465px] top-[94px] absolute" src="https://via.placeholder.com/400x543" />
+    <img className="w-96 h-96 left-[929px] top-[94px] absolute" src="https://via.placeholder.com/400x543" />
+    <div className="w-96 h-28 left-0 top-[517px] absolute bg-black/opacity-80" />
+    <div className="w-96 h-28 left-[465px] top-[517px] absolute bg-black/opacity-80" />
+    <div className="w-96 h-28 left-[929px] top-[517px] absolute bg-black/opacity-80" />
+    <div className="w-80 left-[34px] top-[535px] absolute text-center text-white text-2xl font-semibold font-['Inter']">Find Authentic Phulkari Duppatte From the sellers of Amritsar</div>
+    <div className="w-80 left-[499px] top-[535px] absolute text-center text-white text-2xl font-semibold font-['Inter']">Get Traditional Chikankari suits directly from the artisans of Lucknow</div>
+    <div className="w-80 left-[963px] top-[535px] absolute text-center text-white text-2xl font-semibold font-['Inter']">Get your hands on the Best Pashmina shawls from Kashmir</div>
+  </div>
+  <div className="w-96 h-96 left-[65px] top-[2905px] absolute bg-purple-900" />
+  <div className="w-96 left-[129px] top-[3097px] absolute text-center text-pink-500 text-4xl font-bold font-['Inter']">Fill in the weekly customer survey</div>
+</div>
+    )
+}
